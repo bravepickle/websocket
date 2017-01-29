ba9001c0f3b47581870acb9409a78f48c839fa7e
Victor Konovalov <viktor.konovalov@mads.com>
2017-01-30 01:36:58 +0200

    adding reconnect for client side; adding command-line emulator example

--- README.md
+++ README.md
@@ -4,11 +4,11 @@ Test scripts for web sockets implementation in GOLANG
 ## Features
 * server-side implementation in Go that responds with requested date and extra set of fields
 * client-side (web browser) implemenation of websockets that connect to server
+* reconnect logic after disconnection
 
 ## TODO
 * support of WSS (over HTTPS) protocol
 * proper handling of various specifics of web browser clients handling web sockets
 * add tests
-* reconnect logic
 * connection status check
 * command line commands execution example
--- main.go
+++ main.go
@@ -9,8 +9,9 @@ import (
 	"errors"
 	"sync/atomic"
 	"time"
+	"os/exec"
+	"strings"
 )
-
 
 type responseMsgType struct {
 	Id      uint32 `json:"transactionId"`
@@ -32,6 +33,7 @@ var TransactionId uint32 = 0
 
 func main() {
 	http.Handle("/api", websocket.Handler(Echo))
+	http.Handle("/cmd", websocket.Handler(EchoCmd))
 
 	log.Printf(`Server listening at: "%s"%s`, serverAddr, "\n")
 
@@ -41,16 +43,16 @@ func main() {
 }
 
 func ParseRequestMsg(ws *websocket.Conn) (requestMsg requestMsgType, err error) {
-	var reply string
+	var request string
 
-	if err = websocket.Message.Receive(ws, &reply); err != nil {
-		return requestMsg, errors.New(fmt.Sprint("Can't receive: ", err.Error()))
+	if err = websocket.Message.Receive(ws, &request); err != nil {
+		return requestMsg, errors.New(fmt.Sprint("Can't receive: ", err))
 	}
 
-	fmt.Println("Received back from client: " + reply)
+	fmt.Println("Received from client: " + request)
 
-	if err = json.Unmarshal([]byte(reply), &requestMsg); err != nil {
-		return requestMsg, errors.New(fmt.Sprintf("Failed to decode %s with message: %s\n", reply, err))
+	if err = json.Unmarshal([]byte(request), &requestMsg); err != nil {
+		return requestMsg, errors.New(fmt.Sprintf("Failed to decode %s with message: %s\n", request, err))
 	}
 
 	fmt.Println(`Parsed to`, requestMsg)
@@ -105,4 +107,102 @@ func Echo(ws *websocket.Conn) {
 			log.Println("Can't send:", err)
 		}
 	}
+}
+
+func EchoCmd(ws *websocket.Conn) {
+	for {
+		requestMsg, err := ParseCommand(ws)
+
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+
+		responseMsg, err := GenerateResponseCmd(&requestMsg)
+
+		if err != nil {
+			log.Println(`Command error:`, err)
+			//continue // send response with error to client
+		}
+
+		msg, err := json.Marshal(responseMsg)
+		if err != nil {
+			log.Println(`Encoding error:`, err)
+			continue
+		}
+
+		if sleepSecs > 0 {
+			//time.Sleep(sleepSecs * time.Second)
+		}
+
+		if err = websocket.Message.Send(ws, string(msg)); err != nil {
+			log.Println("Can't send:", err)
+		}
+	}
+}
+
+type cmdType struct {
+	Command string `json:"command"`
+}
+
+func ParseCommand(ws *websocket.Conn) (cmd cmdType, err error) {
+	var request string
+
+	if err = websocket.Message.Receive(ws, &request); err != nil {
+		return cmd, errors.New(fmt.Sprint("Can't receive: ", err))
+	}
+
+	fmt.Println("Received from client: " + request)
+
+	if err = json.Unmarshal([]byte(request), &cmd); err != nil {
+		return cmd, errors.New(fmt.Sprintf("Failed to decode \"%s\" with message: %s\n", request, err))
+	}
+
+	fmt.Println(`Parsed to`, cmd)
+
+	return cmd, nil
+}
+
+type cmdResponseType struct {
+	Id      uint32 `json:"transactionId"`
+	Command cmdType `json:"request"`
+	Output  string `json:"output"`
+	Created string `json:"created"`
+	Server  string `json:"server"`
+	Error   string `json:"error"`
+}
+
+func GenerateResponseCmd(cmd *cmdType) (cmdResponse cmdResponseType, err error) {
+	atomic.AddUint32(&TransactionId, 1)
+	cmdResponse.Id = TransactionId
+	cmdResponse.Server = serverName
+	cmdResponse.Created = time.Now().Format(time.RFC3339)
+	cmdResponse.Command = *cmd
+
+	arrChunks := strings.Split(cmd.Command, ` `)
+	cmdObj := exec.Command(arrChunks[0])
+	if len(arrChunks) > 1 {
+		cmdObj.Args = arrChunks
+		log.Println(`Adding chunks:`, cmdObj.Args)
+	}
+	log.Println(`Command:`, cmdResponse)
+	output, err := cmdObj.Output()
+
+	log.Println(`Command executed:`, cmdObj)
+	if err != nil {
+		cmdResponse.Error = err.Error()
+		return cmdResponse, err
+	}
+
+	cmdResponse.Output = string(output)
+
+	msg, err := json.Marshal(cmdResponse)
+	if err != nil {
+		cmdResponse.Error = err.Error()
+		return cmdResponse, errors.New(fmt.Sprintf("Failed to encode \"%v\" with message: %s\n", cmdResponse, err))
+	}
+
+	fmt.Println("Sending to client: " + string(msg))
+
+	return cmdResponse, nil
 }
